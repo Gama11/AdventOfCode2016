cd02d337d06977de96e980195ffbadab69f238de
Jens Fischer <jensfischer95@gmail.com>
2018-11-08 17:08:33 +0100

    Tests for day 1 part 1

---
+++ src/Day1.hx
@@ -0,0 +1,5 @@
+class Day1 {
+	public static function getDistanceToHQ(sequence:String):Int {
+		return 0;
+	}
+}
--- src/Tests.hx
+++ src/Tests.hx
@@ -8,7 +8,15 @@ class Tests {
 
 	function new() {}
 
-	function testSomething() {
-		Assert.isTrue(false);
+	function getData(name:String):String {
+		return sys.io.File.getContent('data/$name.txt');
+	}
+
+	function testDay1() {
+		Assert.equals(5, Day1.getDistanceToHQ("R2, L3"));
+		Assert.equals(2, Day1.getDistanceToHQ("R2, R2, R2"));
+		Assert.equals(12, Day1.getDistanceToHQ("R5, L5, R5, R3"));
+
+		Assert.equals(-1, Day1.getDistanceToHQ(getData("day1")));
 	}
 }
