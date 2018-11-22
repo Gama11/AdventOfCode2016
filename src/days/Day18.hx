d158da7f08421993e7bd528579cdd4d3c61d12cf
Jens Fischer <jensfischer95@gmail.com>
2018-11-22 13:48:34 +0100

    Improve readability a bit

--- src/days/Day18.hx
+++ src/days/Day18.hx
@@ -7,15 +7,20 @@ class Day18 {
 			var previousRow = grid[grid.length - 1];
 			var newRow = [];
 			for (i in 0...previousRow.length) {
-				var left = previousRow[i - 1];
-				var center = previousRow[i];
-				var right = previousRow[i + 1];
-				var isTrap = (
-					(left == Trap && center == Trap && right != Trap) ||
-					(left != Trap && center == Trap && right == Trap) ||
-					(left == Trap && center != Trap && right != Trap) ||
-					(left != Trap && center != Trap && right == Trap));
-				newRow.push(if (isTrap) Trap else Safe);
+				inline function getTile(i:Int) {
+					var tile = previousRow[i];
+					return if (tile == null) Safe else tile;
+				}
+				var left = getTile(i - 1);
+				var center = getTile(i);
+				var right = getTile(i + 1);
+				newRow.push(switch [left, center, right] {
+					case [Trap, Trap, Safe] |
+						 [Safe, Trap, Trap] |
+						 [Trap, Safe, Safe] |
+						 [Safe, Safe, Trap]: Trap;
+					case _: Safe;
+				});
 			}
 			grid.push(newRow);
 		}
